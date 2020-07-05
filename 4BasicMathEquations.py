439468c86e2026fd4d25fe504f863a597ed2c081
Chippery7 <59151348+Chippery7@users.noreply.github.com>
2020-07-05 17:52:38 -0400

    The sum of the math equation solvers all-in-one project was used from the deleted code that complicated the other individual equation solvers

---
+++ 4BasicMathEquations.py
@@ -0,0 +1,31 @@
+def d():
+    x = input('What is the first number of your problem?')
+    y = input('What is the second number of your problem?')
+    x = int(x)
+    y = int(y)
+    answer = x/y
+    print(answer)
+
+def m():
+    x = input('What is the first number of your problem?')
+    y = input('What is the second number of your problem?')
+    x = int(x)
+    y = int(y)
+    answer = x*y
+    print(answer)
+
+def s():
+    x = input('What is the first number of your problem?')
+    y = input('What is the second number of your problem?')
+    x = int(x)
+    y = int(y)
+    answer = x-y
+    print(answer)
+
+def a():
+    x = input('What is the first number of your problem?')
+    y = input('What is the second number of your problem?')
+    x = int(x)
+    y = int(y)
+    answer = x+y
+    print(answer)
---
+++ SubMath.py
@@ -0,0 +1,6 @@
+x = input('What is the first number of your problem?')
+y = input('What is the second number of your problem?')
+x = int(x)
+y = int(y)
+answer = x-y
+print(answer)
